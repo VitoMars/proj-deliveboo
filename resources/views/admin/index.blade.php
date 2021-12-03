d23edfffe916986c888c66c536e8fad222696531
Vito <vito95v@gmail.com>
2021-12-03 16:05:09 +0100

    piccola modifica

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,12 +27,14 @@
         {{-- Lista Ristoranti --}}
         <div class="col-md-12">
 
+            @if (!$restaurant)
             <a href="{{ route('admin.restaurants.create') }}">
                 <button type="button" class="my-btn-blue btn my-4 text-white d-flex align-items-center">
                     <i class="fas fa-plus pe-2" style=""></i>
                     Aggiungi Ristorante
                 </button>
             </a>
+            @endif
 
             <div class="card mb-3">
                 <div class="card-header pr-0 pl-0">
@@ -40,24 +42,21 @@
                 </div>
 
                 @if( $restaurant )
-
-                    <ul class="m-0">
-                        <li class="m-3">
-                            {{-- Show --}}
-                            <div class="h5 d-flex">
-
-                                <a href="{{route('admin.restaurants.show', $restaurant['id'])}}">{{$restaurant["name"]}}</a>
-                            </div>
-                            {{-- Edit --}}
-                            <a class="btn btn-outline-info" data-mdb-ripple-color="dark"
-                                href="{{ route('admin.restaurants.edit', $restaurant['id']) }}" class="card-link">
-                                <i class="far fa-edit me-2"></i>Modifica
-                            </a>
-                        </li>
-                    </ul>
-                @else 
-
-                    Non hai ancora creato un ristorante! 
+                <ul class="m-0">
+                    <li class="m-3">
+                        {{-- Show --}}
+                        <div class="h5 d-flex">
+                            <a href="{{route('admin.restaurants.show', $restaurant['id'])}}">{{$restaurant["name"]}}</a>
+                        </div>
+                        {{-- Edit --}}
+                        <a class="btn btn-outline-info" data-mdb-ripple-color="dark"
+                            href="{{ route('admin.restaurants.edit', $restaurant['id']) }}" class="card-link">
+                            <i class="far fa-edit me-2"></i>Modifica
+                        </a>
+                    </li>
+                </ul>
+                @else
+                <div class="m-2">Non hai ancora creato un ristorante!</div>
                 @endif
             </div>
         </div>
